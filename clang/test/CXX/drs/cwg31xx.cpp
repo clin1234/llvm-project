3dedd3e7ede159918b518539d7b479bc33066758
Matheus Izvekov <mizvekov@gmail.com>
2026-04-04 20:44:35 -0300

    [clang] implement CWG2064: ignore value dependence for decltype
    The 'decltype' for a value-dependent (but non-type-dependent) should be known,
    so this patch makes them non-opaque instead.
    
    This patch also implements what's neceessary to allow overloading
    on pure differences in instantiation dependence, making `std::void_t`
    usable for SFINAE purposes.
    
    This also readds a few test cases from da98651, which was a previous attempt
    at resolving CWG2064.
    
    Fixes #8740
    Fixes #61818
    Fixes #190388

--- clang/test/CXX/drs/cwg31xx.cpp
+++ clang/test/CXX/drs/cwg31xx.cpp
@@ -1,4 +1,4 @@
-// RUN: %clang_cc1 -std=c++98 -fexceptions -fcxx-exceptions -pedantic-errors %s -verify-directives -verify=expected,cxx98-17
+// RUN: %clang_cc1 -std=c++98 -fexceptions -fcxx-exceptions -pedantic-errors %s -verify-directives -verify=expected,cxx98,cxx98-17
 // RUN: %clang_cc1 -std=c++11 -fexceptions -fcxx-exceptions -pedantic-errors %s -verify-directives -verify=expected,cxx98-17
 // RUN: %clang_cc1 -std=c++14 -fexceptions -fcxx-exceptions -pedantic-errors %s -verify-directives -verify=expected,cxx98-17
 // RUN: %clang_cc1 -std=c++17 -fexceptions -fcxx-exceptions -pedantic-errors %s -verify-directives -verify=expected,cxx98-17
@@ -6,7 +6,7 @@
 // RUN: %clang_cc1 -std=c++23 -fexceptions -fcxx-exceptions -pedantic-errors %s -verify-directives -verify=expected,since-cxx20
 // RUN: %clang_cc1 -std=c++2c -fexceptions -fcxx-exceptions -pedantic-errors %s -verify-directives -verify=expected,since-cxx20
 
-// cxx98-17-no-diagnostics
+// cxx98-no-diagnostics
 
 namespace cwg3106 { // cwg3106: 2.7
 #if __cplusplus >= 201103L
@@ -45,3 +45,22 @@ decltype([b = C(3)](){ return 4; }()) x;
 } // namespace cwg3156
 
 // cwg3172: na
+
+namespace cwg3179 { // cwg3179: 23 tentatively ready 2026-04-30
+#if __cplusplus >= 201103L
+  template<class> using void_t = void;
+  template<class T> struct S {
+    void f(void_t<T*>);
+    // expected-error@-1 {{'void' as parameter must not involve template parameters}}
+
+    using X = int(void_t<T*>);
+    // expected-error@-1 {{'void' as parameter must not involve template parameters}}
+  };
+  template<class T> void g(decltype((void)(T*)0));
+  // expected-error@-1 {{'void' as parameter must not involve template parameters}}
+#endif
+#if __cplusplus >= 202002L
+  template<class T> bool v = requires(void_t<T>) { true; };
+  // expected-error@-1 {{'void' as parameter must not involve template parameters}}
+#endif
+} // namespace cwg3179
